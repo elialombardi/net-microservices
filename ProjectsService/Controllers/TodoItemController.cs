ef531cc63f4cbd0282078fd7001d3d0e7957c05e
Elia Lombardi <elia.lombardi@outlook.it>
2021-12-24 07:40:23 +0100

    working on projects service

--- ProjectsService/Controllers/TodoItemController.cs
+++ ProjectsService/Controllers/TodoItemController.cs
@@ -1,27 +1,25 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
-using ProjectsService.Data;
-using ProjectsService.Dtos;
-using ProjectsService.Models;
 
 namespace ProjectsService.Controllers
 {
-    [Route("api/p/todo-items")]
-    [ApiController]
-    public class TodoItemController : ControllerBase
+  [Route("api/p/todo-items")]
+  [ApiController]
+  public class TodoItemController : ControllerBase
+  {
+
+    public TodoItemController()
     {
-
-        public TodoItemController()
-        {
-        }
-
-        [HttpGet]
-        public ActionResult TestInboundConnection()
-        {
-            Console.WriteLine("--> Inbound connection successfull");
-            return Ok("Inbound connection successfull");
-        }
     }
+
+    [HttpPost]
+    public ActionResult TestInboundConnection()
+    {
+      Console.WriteLine("--> Inbound connection successfull");
+      return Ok("Inbound connection successfull");
+    }
+  }
 }
